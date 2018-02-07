c4e6903f7939386cc1638d85769dad66c7edc385
GyrateDark <GyrateDark@users.noreply.github.com>
2018-02-07 14:06:41 -0600

    Initial commit

---
+++ Microcontroller/Lilypad/Lilypad.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  // Setup lilypad
+  // https://www.simblee.com/Simblee%20Functions%20Reference%20V1.0.pdf
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
